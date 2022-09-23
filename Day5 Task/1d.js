ab5e0bdc98a60dd7d8d8337fcb229903af38f03a
Rajendrankr25 <rajendrankr25@gmail.com>
2022-09-23 14:47:24 +0530

    updte

--- Day5 Task/1d.js
+++ Day5 Task/1d.js
@@ -1,21 +1,13 @@
 //Return all the prime numbers in an array
-let a1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let a1 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 let temp = [];
 //Anonymous function
-let prime = function() {
-    for(let i=0; i<a1.length; i++) {
-        if(a1[i]%a1[i]===0) {
-            temp.push(a1[i])
-        } 
+let prime = function () {
+ a1 = a1.filter((number) => {
+    for(var i=2; i<=Math.sqrt(number); i++) {
+        if(number % i === 0) return false;
     }
+    return true;
+})
 }
-prime();
-console.log(temp);
-//IIFE
-(function prime() {
-    for(let i=0; i<a1.length; i++) {
-        if(a1[i]%a1[i]===0) {
-            temp.push(a1[i])
-        } 
-    }
-})(prime(a1))
+console.log(prime(a1));
--- Day5 Task/3d.js
+++ Day5 Task/3d.js
@@ -1,12 +1,9 @@
 //Return all the prime numbers in an array
-let a1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-let temp = [];
-let prime = ()=> {
-    for(let i=0; i<a1.length; i++) {
-        if(a1[i]%a1[i]===0) {
-            temp.push(a1[i])
-        } 
+let a1 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+a1 = a1.filter((number) => {
+     for(var i=2; i<=Math.sqrt(number); i++) {
+         if(number % i === 0) return false;
     }
-}
-prime();
-console.log(temp);
+    return true;
+})
+console.log(a1);
