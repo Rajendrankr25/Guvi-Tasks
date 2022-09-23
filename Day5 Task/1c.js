0a27ab506fc035332694f41e3173a35fb144fac9
Rajendrankr25 <rajendrankr25@gmail.com>
2022-09-23 12:36:47 +0530

    updated

--- Day5 Task/1c.js
+++ Day5 Task/1c.js
@@ -1,6 +1,7 @@
 //Sum of all numbers in an array
 let a1 = [10,20,30,40];
 let res = 0;
+//Anonymous function
 let sum = function() {
     for(let i=0; i<a1.length; i++) {
         res +=a1[i];
@@ -8,3 +9,9 @@ let sum = function() {
 }
 sum();
 console.log(res);
+//IIFE
+(function sum() {
+    for(let i=0; i<a1.length; i++) {
+        res +=a1[i];
+}
+})(sum(a1))
--- Day5 Task/1d.js
+++ Day5 Task/1d.js
@@ -1,6 +1,7 @@
 //Return all the prime numbers in an array
 let a1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 let temp = [];
+//Anonymous function
 let prime = function() {
     for(let i=0; i<a1.length; i++) {
         if(a1[i]%a1[i]===0) {
@@ -10,3 +11,11 @@ let prime = function() {
 }
 prime();
 console.log(temp);
+//IIFE
+(function prime() {
+    for(let i=0; i<a1.length; i++) {
+        if(a1[i]%a1[i]===0) {
+            temp.push(a1[i])
+        } 
+    }
+})(prime(a1))
