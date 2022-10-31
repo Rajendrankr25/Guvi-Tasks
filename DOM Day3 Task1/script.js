a54c4acf9f5bae062f2d2cfa34cadbe948ba3826
Rajendrankr25 <rajendrankr25@gmail.com>
2022-10-31 14:31:56 +0530

    update

--- DOM Day3 Task1/script.js
+++ DOM Day3 Task1/script.js
@@ -57,7 +57,8 @@ var div3 = tagWithClass("div", "class", "col-md-3");
 var div4 = tagWithClass("div", "class", "col-md-9");
 var h1 = tagWithClassContent("h1", "id", "title", "Form Submission");
 var p1 = tagWithClassContent("p", "id", "description", "DOM Manipulation with Forms");
-var form = tagWithClass("form", "id", "form");
+var form = tagWithClassId("form", "class","survey-form","id", "survey-form");
+form.setAttribute("onsubmit", '');
 div.append(div1);
 div1.append(div2);
 div2.append(div3, div4);
@@ -65,6 +66,7 @@ div3.append(h1, p1, form);
 var lbl1 = tagWithClassContent("label", "for", "first-name", "First Name :");
 var br1 = tag("br");
 var inp1 = tagWithClassIdPlace("input", "type", "text", "id", "first-name", "placeholder", "First Name");
+inp1.setAttribute("name", "first-name");
 var br2 = tag("br");
 var br3 = tag("br");
 var lbl2 = tagWithClassContent("label", "for", "last-name", "Last Name :");
@@ -85,8 +87,10 @@ var br10 = tag("br");
 var lbl5 = tagWithClassContent("label", "for", "gender", "Gender :");
 var br11 = tag("br");
 var inp4 = tagWithClassIdPlace("input", "type", "radio", "name", "gender", "id", "male");
+inp4.setAttribute("value", "Male");
 var lbl6 = tagWithClassContent("label", "for", "male", "&nbspMale&nbsp");
 var inp5 = tagWithClassIdPlace("input", "type", "radio", "name", "gender", "id", "female");
+inp5.setAttribute("value", "Female");
 var lbl7 = tagWithClassContent("label", "for", "female", "&nbspFemale&nbsp");
 var br12 = tag("br");
 var br13 = tag("br");
@@ -94,18 +98,23 @@ var lbl8 = tagWithClassContent("label", "for", "food", "Choice of Food :");
 var sm1 = tagWithContent("small", " (must choose atleast 2 out of 5)");
 var br14 = tag("br");
 var inp6 = tagWithClassIdPlace("input", "type", "checkbox", "name", "food", "id", "indian");
+inp6.setAttribute("value", "indian");
 var lbl9 = tagWithClassContent("label", "for", "indian", "&nbspIndian");
 var br15 = tag("br");
 var inp7 = tagWithClassIdPlace("input", "type", "checkbox", "name", "food", "id", "italian");
+inp7.setAttribute("value", "italian");
 var lbl10 = tagWithClassContent("label", "for", "italian", "&nbspItalian");
 var br16 = tag("br");
 var inp8 = tagWithClassIdPlace("input", "type", "checkbox", "name", "food", "id", "american");
+inp8.setAttribute("value", "american");
 var lbl11 = tagWithClassContent("label", "for", "american", "&nbspAmerican");
 var br17 = tag("br");
 var inp9 = tagWithClassIdPlace("input", "type", "checkbox", "name", "food", "id", "arabian");
+inp9.setAttribute("value", "arabian");
 var lbl12 = tagWithClassContent("label", "for", "arabian", "&nbspArabian");
 var br18 = tag("br");
 var inp10 = tagWithClassIdPlace("input", "type", "checkbox", "name", "food", "id", "chinese");
+inp10.setAttribute("value", "chinese");
 var lbl13 = tagWithClassContent("label", "for", "chinese", "&nbspChinese");
 var br19 = tag("br");
 var br20 = tag("br");
@@ -120,7 +129,7 @@ var inp12 = tagWithClassIdPlace("input", "type", "text", "id", "country", "place
 var br25 = tag("br");
 var br26 = tag("br");
 var btn1 = tagWithClassIdContent("button", "class", "btn btn-primary", "id", "submit", "Submit");
-btn1.addEventListener("click",genderChoose);
+btn1.addEventListener("click", dataRetrive);
 var br27 = tag("br");
 var br28 = tag("br");
 
@@ -134,9 +143,7 @@ var table = tagWithClass("table", "class", "table");
 div4.append(h2, table);
 var thead = tagWithClass("thead", "class", "thead-dark");
 var tbody = tag("tbody");
-table.append(thead,tbody);
-var tr1 = tag("tr");
-thead.append(tr1);
+table.append(thead, tbody);
 var thd1 = tagWithClassContent("th", "scope", "col", "First Name");
 var thd2 = tagWithClassContent("th", "scope", "col", "Last Name");
 var thd3 = tagWithClassContent("th", "scope", "col", "Address");
@@ -145,28 +152,69 @@ var thd5 = tagWithClassContent("th", "scope", "col", "Gender");
 var thd6 = tagWithClassContent("th", "scope", "col", "Food");
 var thd7 = tagWithClassContent("th", "scope", "col", "State");
 var thd8 = tagWithClassContent("th", "scope", "col", "Country");
-tr1.append(thd1, thd2, thd3, thd4, thd5, thd6, thd7, thd8);
+thead.append(thd1, thd2, thd3, thd4, thd5, thd6, thd7, thd8);
 document.body.append(div);
 
-//Interaction;
-
-function genderChoose(){
-    if(male.checked){
-        let res = document.getElementById("male").value;
-        console.log(res);
+//Gender Choose Function;
+var ml, fml;
+function genderChoose(ml, fml) {
+    if (male.checked) {
+        let ml = document.getElementById("male").value;
+        return ml;
+    } else if (female.checked) {
+        let fml = document.getElementById("female").value;
+        return fml;
     } else {
-        let res1 = document.getElementById("female").value;
-        console.log(res1);
+        alert("Choose Gender");
     }
 }
 
-function dataRetrive(){
-    var fn = document.getElementById("first-name");
-    var ln = document.getElementById("last-name");
-    var adrs = document.getElementById("address");
-    var pin = document.getElementById("pincode");
-    var gndr = genderChoose();
-    return gndr;
-
+//Food Choose Function;
+var temp = [];
+var food;
+function foodChoose(food) {
+    let fd = document.getElementsByTagName("checkbox").checked;
+    temp.push(fd);
+    if (temp.length >= 2) {
+        food = [...temp];
+        return food;
+    } else {
+        alert("Must choose atleast 2 food choices");
+    }
 }
-//sconsole.log(gndr);
+
+//Function for Appending to Table;
+function dataRetrive(event) {
+    event.preventDefault();
+    
+    var fn = document.getElementById("first-name").value;
+    var ln = document.getElementById("last-name").value;
+    var adrs = document.getElementById("address").value;;
+    var pin = document.getElementById("pincode").value;
+    var gndr = genderChoose(ml, fml);
+    var cfood = foodChoose(food);
+    var ste = document.getElementById("state").value;
+    var cntry = document.getElementById("country").value;
+    console.log(`First Name: ${fn}, Last Name: ${ln}, Address: ${adrs}, Pincode: ${pin}, Gender: ${gndr}, Choice of Food: ${cfood}, State: ${ste}, Country: ${cntry}`);
+//Creating table data;
+    var trow = document.createElement("tr");
+    var td1 = document.createElement("td");
+    td1.innerHTML = fn;
+    var td2 = document.createElement("td");
+    td2.innerHTML = ln;
+    var td3 = document.createElement("td");
+    td3.innerHTML = adrs;
+    var td4 = document.createElement("td");
+    td4.innerHTML = pin;
+    var td5 = document.createElement("td");
+    td5.innerHTML = gndr;
+    var td6 = document.createElement("td");
+    td6.innerHTML = cfood;
+    var td7 = document.createElement("td");
+    td7.innerHTML = ste;
+    var td8 = document.createElement("td");
+    td8.innerHTML = cntry;
+    trow.append(td1, td2, td3, td4, td5, td6, td7, td8);
+    tbody.append(trow);
+    document.getElementById("survey-form").reset();
+}
